dea5dabd7b57bb511e3fc1d0e15ce78496a21262
WEATHERYOU <131228360+WEATHERYOU@users.noreply.github.com>
2023-11-09 21:17:34 +0700

    Add files via upload

---
+++ TracuuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLDATXE.Controllers
+{
+    public class TracuuController : Controller
+    {
+        // GET: Tracuu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KetQuaTraCuu(string maVe)
+        {
+            var thongTinVe = TicketController(maVe); // Thay thế bằng cách truy vấn thực tế của bạn
+            return View(thongTinVe);
+        }
+
+    }
+}
